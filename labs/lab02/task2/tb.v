83abdfa2ad7b578e45e60d39c069adb1874d93fa
f20250789-oss <f20250789@pilani.bits-pilani.ac.in>
2026-09-15 16:09:22 +0000

    lab02 task2

--- labs/lab02/task2/tb.v
+++ labs/lab02/task2/tb.v
@@ -4,9 +4,15 @@
 module tb;
 
   // TODO: declare the inputs and outputs
+  parameter WIDTH = 8;
+  parameter DEPTH = 4;
+  reg [$clog2(DEPTH)-1:0] t_sel;
+  wire [WIDTH-1:0]         t_dout;
 
   // TODO: instantiate DUT here
-
+  lut #(.WIDTH(WIDTH), .DEPTH(DEPTH)) DUT(
+    .sel(t_sel), .dout(t_dout)
+  );
   // Waveform dump configuration (DO NOT CHANGE)
   string vcd_file;
   initial begin
@@ -18,10 +24,14 @@ module tb;
 
   initial begin
     // TODO: apply different input combinations
-
+    t_sel = 0;
+    #10 t_sel = 1;
+    #10 t_sel = 2;
+    #10 t_sel = 3;
+    #10 $finish;
   end
 
   initial
-    $monitor($time, " I0=%b I1=%b S=%b | Y=%b", t_i0, t_i1, t_s, t_y); // change as required
+    $monitor($time, " sel=%b | dout=%d", t_sel, t_dout); // change as required
 
 endmodule
